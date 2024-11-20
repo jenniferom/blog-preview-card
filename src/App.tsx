861e978602ec48a8c238d0d5660e5be019d4a53c
jenniferom <jenniferom0009@gmail.com>
2024-11-20 16:17:58 -0500

    style: improving css

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,21 @@ function App() {
 
   return (
     <div className='container'>
-      <img className='image' src={Image} alt="" />
+      <img className='image' src={Image} />
 
       <div className='text'>
-        <strong>Learning</strong>
+        <p className='p1'>Learning</p>
 
         <p className='date'>Published 21 Dec 2023</p>
 
         <h2>HTML & CSS foundations</h2>
 
-        <p >These languages are the backbone of every website, defining structure, content, and presentation.</p>
+        <p style={{color: 'gray'}}>These languages are the backbone of every website, defining structure, content, and presentation.</p>
 
       </div>
       <div className="avatar">
         <img className='avatar-image' src="/images/image-avatar.webp" />
-        <h3>Greg Hooper</h3>
+        <h4>Greg Hooper</h4>
       </div>
     </div>
   )
